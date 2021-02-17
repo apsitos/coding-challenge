f9a10e144394ad4b73eedbb384a1c60aedfac2cf
Annastasia Psitos <annastasia.psitos@gmail.com>
2021-02-16 20:28:40 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      },
+      {
+        "test": /\.css$/,
+        "use": [
+            "style-loader",
+            "css-loader"
+        ]
+      }
+    ]
+  },
+  devServer: {
+      contentBase: path.join(__dirname, 'src')
+  }
+};
